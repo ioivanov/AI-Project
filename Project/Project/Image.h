70900be1727446c286b262c34d910a529dafe00f
Ivailo Ivanov <ivaylo.ivanov.95@hotmail.com>
2018-02-01 22:50:49 +0200

    new file:   Project/Project/Image.h 	new file:   Project/Project/Project.cpp 	new file:   Project/Project/Project.vcxproj 	new file:   Project/Project/Project.vcxproj.filters 	new file:   Project/Project/ReadMe.txt 	new file:   Project/Project/stdafx.cpp 	new file:   Project/Project/stdafx.h 	new file:   Project/Project/targetver.h

---
+++ Project/Project/Image.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Image
+{
+	char array[2025]; // pixels for image with size 45x45
+	int type; // class of the image
+
+public:
+	//read image
+
+	//write image
+
+};
